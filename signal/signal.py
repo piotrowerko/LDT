45c7dab358a9b934eb22ec5f6ce477120ef6bccb
Tomasz Owerko <50355739+owerko@users.noreply.github.com>
2020-11-02 12:50:57 +0100

    signal class (#4)

---
+++ signal/signal.py
@@ -0,0 +1,44 @@
+import numpy as np
+import statistics
+from scipy.signal import hilbert
+from scipy.optimize import least_squares
+
+
+class Signal:
+    def __init__(self, name):
+        self.name = name
+        self.time = np.zeros(shape=(1, 1), dtype=np.float64)
+        self.Amplitude = np.zeros(shape=(1, 1), dtype=np.float64)
+
+    def fun(sefl, x, t, y):
+        return x[0] * np.exp(-x[1] * t) + x[2] - y
+
+    def load_data(self, filePath):
+        with open(filePath) as f:
+            data = np.loadtxt(f, dtype=np.float64)
+            t = data[:, 0]
+            A = data[:, 1]
+            A2 = A - statistics.mean(A)
+            loc_max_A2_begin_ind = np.argmax(A2[:int(0.1 * len(t))])
+            loc_max_A2_end_ind = np.argmax(A2[int(0.9 * len(t)):]) + len(A2[:int(0.9 * len(t))])
+            t2 = t[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
+            self.time = t2 - t2[0]
+            self.Amplitude = A2[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
+
+    def lsq_resutls(self, t, A):
+        amplitude_envelope = np.abs(hilbert(A))
+        x0 = np.array([1.0, 1.0, 1.0])
+        res_lsq = least_squares(self.fun, x0, args=(t, amplitude_envelope))
+        return res_lsq
+
+    def softL1_results(self, t, A):
+        amplitude_envelope = np.abs(hilbert(A))
+        x0 = np.array([1.0, 1.0, 1.0])
+        res_soft_l1 = least_squares(self.fun, x0, loss='soft_l1', f_scale=0.1, args=(t, amplitude_envelope))
+        return res_soft_l1
+
+    def huber_results(self, t, A):
+        amplitude_envelope = np.abs(hilbert(A))
+        x0 = np.array([1.0, 1.0, 1.0])
+        res_hub = least_squares(self.fun, x0, loss='huber', f_scale=0.1, args=(t, amplitude_envelope))
+        return res_hub
