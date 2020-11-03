e4e5aa933fc7996a89761ba83f53e83d76d693ed
Piotr Owerko <63237588+piotrowerko@users.noreply.github.com>
2020-11-03 14:10:04 +0100

    korekty signal i signal dojo, dodanie nowej funkcji (#7)
    * drobne korekty na szybko - testuje upstream - Piotr Owerko
    
    * korekty signal i signal dojo + nowa fukcja

--- signal/signal.py
+++ signal/signal.py
@@ -1,6 +1,5 @@
-import numpy as np
-import statistics
-from scipy.signal import hilbert
+import numpy as np  # usunięcie importu statistics
+from scipy.signal import hilbert 
 from scipy.optimize import least_squares
 
 
@@ -8,8 +7,9 @@ class Signal:
     def __init__(self, name):
         self.name = name
         self.x = np.array([1.0, 1.0, 1.0])
-        self.time = np.zeros(shape=(1, 1), dtype=np.float64)
-        self.Amplitude = np.zeros(shape=(1, 1), dtype=np.float64)
+        self.time = np.zeros(shape=(1), dtype=np.float64)  # zminimalizowanie wymiarów wartości domyślnej atrybutu np. array z 1,1 do 1
+        self.Amplitude = np.zeros(shape=(1), dtype=np.float64)
+        self.amplitude_envelope = np.zeros(shape=(1), dtype=np.float64)  # dodanie atrybutu
 
     def __str__(self):
         return f'{self.name}'
@@ -18,29 +18,38 @@ class Signal:
         return x[0] * np.exp(-x[1] * t) + x[2] - y
 
     def load_data(self, filePath):
-        with open(filePath) as f:
-            data = np.loadtxt(f, dtype=np.float64)
-            t = data[:, 0]
-            A = data[:, 1]
-            A2 = A - statistics.mean(A)
-            loc_max_A2_begin_ind = np.argmax(A2[:int(0.1 * len(t))])
-            loc_max_A2_end_ind = np.argmax(A2[int(0.9 * len(t)):]) + len(A2[:int(0.9 * len(t))])
-            t2 = t[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
-            self.time = t2 - t2[0]
-            self.Amplitude = A2[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
+        data = np.loadtxt(filePath, dtype=np.float64)  # usunięcie podowojenia w imporcie pliku
+        t = data[:, 0]
+        A = data[:, 1]
+        A2 = A - np.mean(A)  # korzystam z numpaja, skoro i tak musi być
+        loc_max_A2_begin_ind = np.argmax(A2[:int(0.1 * len(t))])
+        loc_max_A2_end_ind = np.argmax(A2[int(0.9 * len(t)):]) + len(A2[:int(0.9 * len(t))])
+        t2 = t[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
+        self.time = t2 - t2[0]
+        self.Amplitude = A2[loc_max_A2_begin_ind:loc_max_A2_end_ind:1]
+        self.amplitude_envelope = np.abs(hilbert(self.Amplitude))
+    
+    def compute_sampling_spacing(self): #dodanie nowej fukncji
+        """Computes key parameters of sampling features of the input signal from in-situ device"""
+        time_diffs = np.diff(self.time[:])
+        mean_of_time_diffs = np.mean(time_diffs)
+        median_of_time_diffs = np.median(time_diffs)
+        std_of_time_diffs = np.std(time_diffs)  # standard deviation 
+        return [mean_of_time_diffs, median_of_time_diffs, std_of_time_diffs]
+
+    def compute_period(self):
+        """Computes period of structural response based on dft"""
+        pass  # do zrobienia
 
     def lsq_resutls(self):
-        amplitude_envelope = np.abs(hilbert(self.Amplitude))
-        res_lsq = least_squares(self.fun, self.x, args=(self.time, amplitude_envelope))
+        res_lsq = least_squares(self.fun, self.x, args=(self.time, self.amplitude_envelope)) # usunięcie genrowania zmiennej lokalnej obwiedni
         return res_lsq
 
     def softL1_results(self):
-        amplitude_envelope = np.abs(hilbert(self.Amplitude))
-        res_soft_l1 = least_squares(self.fun, self.x, loss='soft_l1', f_scale=0.1, args=(self.time, amplitude_envelope))
+        res_soft_l1 = least_squares(self.fun, self.x, loss='soft_l1', f_scale=0.1, args=(self.time, self.amplitude_envelope)) # jw
         return res_soft_l1
 
     def huber_results(self):
-        amplitude_envelope = np.abs(hilbert(self.Amplitude))
-        res_hub = least_squares(self.fun, self.x, loss='huber', f_scale=0.1, args=(self.time, amplitude_envelope))
+        res_hub = least_squares(self.fun, self.x, loss='huber', f_scale=0.1, args=(self.time, self.amplitude_envelope))  # jw
         return res_hub
 
--- signal_dojo.py
+++ signal_dojo.py
@@ -2,15 +2,19 @@ from signal.signal import Signal
 
 if __name__ == '__main__':
     # define object
-    myobject = Signal('ztpi')
+    myobject = Signal('\nztpi\n')
     # print object name
     print(myobject)
     #import data
     myobject.load_data('./ztpi_data.txt')
+    #check key parameters of sampling features of the input signal from in-situ device
+    print(f'mean, median, and standard deviation of input sampling rate: \n{myobject.compute_sampling_spacing()}\n')
     #return LSF object cost function
     print(f'{myobject.lsq_resutls().cost}')
     # return Soft_L1 fit object
     print(f'{myobject.softL1_results()}')
     # return Huber fit object's solve vector - first element
     print(f'{myobject.huber_results().x[0]}')
+
+    
 
